894587568e1aa7e832ef8a23fbe32c4901a89a4f
Tom Strat <tomstratfic@hotmail.com>
2021-03-09 12:15:04 +0000

    add code for slide component

--- src/pages/components/slide.js
+++ src/pages/components/slide.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+export default function Slide(props){
+  return (
+    <div>
+      <div>
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>
+        {props.button ? "</slideButton>" : ""}
+      </div>
+    </div>
+  )
+}
