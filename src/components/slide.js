7dbfa877fd9ba00656892ad5872a0b0f4b7182ff
Tom Strat <tomstratfic@hotmail.com>
2021-03-09 12:36:33 +0000

    added slide scoped styles

--- src/components/slide.js
+++ src/components/slide.js
@@ -1,15 +1,16 @@
 import React from "react";
 import SlideButton from "./slideButton";
+import * as slideStyles from "../styles/slide.module.css";
 
 export default function Slide(props){
   //If a button is needed for the slide, this parses the prop and adds.
   const button = props.button ? <SlideButton/> : "";
 
   return (
-    <div>
-      <div>
-        <h1>{props.title}</h1>
-        <h2>{props.subtitle}</h2>
+    <div className={slideStyles.slide}>
+      <div className={slideStyles.slideContent}>
+        <h1 className={slideStyles.title}>{props.title}</h1>
+        <h2 className={slideStyles.subtitle}>{props.subtitle}</h2>
         {button}
       </div>
     </div>
