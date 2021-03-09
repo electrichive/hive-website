c326c7f36cf3f53bd615c15d7a52d56197af6a3e
Tom Strat <tomstratfic@hotmail.com>
2021-03-09 12:50:00 +0000

    added button styles

--- src/components/slide.js
+++ src/components/slide.js
@@ -5,7 +5,7 @@ import * as slideStyles from "./styles/slide.module.css";
 const SlideButton = () => {
   return (
     <div className={slideStyles.button}>
-      <Link to="/Mentorship" className={slideStyles.buttonText}>Join The Hive</Link>
+      <Link to="/mentorship" className={slideStyles.buttonText}>Join The Hive</Link>
     </div>
   );
 };
