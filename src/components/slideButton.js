abce797c5f5e296c2d9231dc9af2e8d76d992126
Tom Strat <tomstratfic@hotmail.com>
2021-03-09 12:29:09 +0000

    add about page with new slide

--- src/components/slideButton.js
+++ src/components/slideButton.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function SlideButton(){
-  <div>
-    <a>Join The Hive</a>
-  </div>
+  return (
+    <div>
+      <a>Join The Hive</a>
+    </div>
+  );
 }
