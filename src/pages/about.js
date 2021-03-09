9b640c53ecbd106fb79d6d098096608d6e3559a7
Tom Strat <tomstratfic@hotmail.com>
2021-03-09 12:29:09 +0000

    add about page with new slide

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Slide from "../components/slide";
+
+export default function About(){
+  return (
+    <Slide title="About Us" subtitle="Subtitle Mission Statement" button="true"/>
+  );
+}
