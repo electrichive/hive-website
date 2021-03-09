cef3fb5043cfdd6f4acca5c41dd9d04cc1b83687
Tom Strat <tomstratfic@hotmail.com>
2021-03-09 12:20:19 +0000

    add button conditional prop

--- src/pages/components/slide.js
+++ src/pages/components/slide.js
@@ -1,12 +1,16 @@
 import React from "react";
+import SlideButton from "./slideButton";
 
 export default function Slide(props){
+  
+  const button = props.button ? <SlideButton/> : ""
+
   return (
     <div>
       <div>
         <h1>{props.title}</h1>
         <h2>{props.subtitle}</h2>
-        {props.button ? "</slideButton>" : ""}
+        {button}
       </div>
     </div>
   )
---
+++ src/pages/components/slideButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SlideButton(){
+  <div>
+    <a>Join The Hive</a>
+  </div>
+}
