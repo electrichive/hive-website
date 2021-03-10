2586830e772bec6eb1e5943a68218958ecf1a861
Tom Strat <tomstratfic@hotmail.com>
2021-03-10 12:09:32 +0000

    added infoBlock into about for testing

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import * as slideStyles from "./button.module.css";
+import * as buttonStyles from "./button.module.css";
 
 /**
  * Component for a standard button (has url, theme and text prop)
@@ -10,10 +10,10 @@ import * as slideStyles from "./button.module.css";
 
 export default function Button(props){
   //two styles of button can be passed through prop
-  const theme = props.theme === "light" ? slideStyles.light : slideStyles.dark;
+  const theme = props.theme === "light" ? buttonStyles.light : buttonStyles.dark;
   
   return (
-      <Link to={props.url} className={`${theme} ${slideStyles.button}`}>
+      <Link to={props.url} className={`${theme} ${buttonStyles.button}`}>
         {props.text}
       </Link>
   );
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,13 @@ import React from "react";
 import Slide from "../components/Slide/slide";
 import Layout from "../components/Layout/layout";
 import Button from "../components/Button/button";
+import * as aboutStyles from "./styles/about.module.css";
 
 //Blocks specific to the about page so created here
 const InfoBlock = (props) => {
-  const theme = props.theme === "light" ? slideStyles.light : slideStyles.dark;
+  const theme = props.theme === "light" ? aboutStyles.light : aboutStyles.dark;
   return (
-    <div>
+    <div className={`${theme} ${aboutStyles.infoBlock}`}>
       <h3>{props.title}</h3>
       <p>{props.text}</p>
       <Button theme={props.buttonTheme} text={props.buttonText} url={props.buttonUrl}/>
@@ -19,6 +20,7 @@ export default function About(){
   return (
     <Layout>
       <Slide title="About Us" subtitle="Subtitle Mission Statement" button="true"/>
+      <InfoBlock theme="light" title="Mentorship" text="Test" buttonTheme="dark" buttonText="Sign Up" buttonUrl="/mentorship" />
     </Layout>
   );
 }
