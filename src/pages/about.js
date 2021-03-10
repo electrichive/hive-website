7e8e65f6b51deaa54a2c66b9af77f3c43d226924
Tom Strat <tomstratfic@hotmail.com>
2021-03-10 11:48:42 +0000

    add more button css and test conditional

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Slide from "../components/Slide/slide";
 import Layout from "../components/Layout/layout";
+import Button from "../components/Button/button";
 
 export default function About(){
   return (
     <Layout>
       <Slide title="About Us" subtitle="Subtitle Mission Statement" button="true"/>
+      <Button theme="dark" text="Join the Hive" url="/foss"/>
     </Layout>
   );
 }
