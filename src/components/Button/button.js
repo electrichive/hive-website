37e0de703318bd270f7efb34a4d6ed5c14899355
Tom Strat <tomstratfic@hotmail.com>
2021-03-10 11:39:47 +0000

    button component built

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Link } from "gatsby"
+import { Link } from "gatsby";
 import * as slideStyles from "./slide.module.css";
 
 
 export default function Button(props){
+  //two styles of button can be passed through prop
+  let theme = props.theme === "light" ? slideStyles.light : slideStyles.dark;
+  
+  return (
+      <Link to={props.url} className={theme}>
+        {props.text}
+      </Link>
+  );
 
 }
