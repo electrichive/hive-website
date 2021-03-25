8e1505c8bcd0c42a1205b14b1949a0260ae95d89
Tom Strat <tomstratfic@hotmail.com>
2021-03-25 10:08:32 +0000

    added boilerplate for testimonial module

--- src/components/Testimonial/testimonial.js
+++ src/components/Testimonial/testimonial.js
@@ -1,3 +1,13 @@
 import React from "react";
 import { Link } from "gatsby";
 import * as testiStyles from "./testimonial.module.css";
+
+/**
+ * Component for a standard testimonial module (has an image name and text prop)
+ * initially puls data from a JSON file
+ * @returns JSXElement 
+ */
+
+export default function Testimonial(props){
+  
+}
