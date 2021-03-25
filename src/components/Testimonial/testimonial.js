cb7ab6b6591f717b072a7468ab32b8cd94f4feac
Tom Strat <tomstratfic@hotmail.com>
2021-03-25 10:14:39 +0000

    testimonial module finalised

--- src/components/Testimonial/testimonial.js
+++ src/components/Testimonial/testimonial.js
@@ -4,10 +4,16 @@ import * as testiStyles from "./testimonial.module.css";
 
 /**
  * Component for a standard testimonial module (has an image name and text prop)
- * initially puls data from a JSON file
+ * is passed data from the testimonials module
  * @returns JSXElement 
  */
 
 export default function Testimonial(props){
-  
+  return (
+    <div className={"testimonialContainer"}>
+      <img src={props.img} className={"testimonialImage"} />
+      <h3 className={"testimonialTitle"}>{props.title}</h3>
+      <p className={"testimonialContent"}>{props.content}</p>
+    </div>
+  );
 }
