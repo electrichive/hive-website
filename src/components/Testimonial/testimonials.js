39d3796f7aeac6663ac3bfeb25866aa2da40d250
Tom Strat <tomstratfic@hotmail.com>
2021-04-04 21:49:41 +0100

    added content to json and imported into component

--- src/components/Testimonial/testimonials.js
+++ src/components/Testimonial/testimonials.js
@@ -2,6 +2,7 @@ import React from "react";#
 import Testimonial from "./testimonial";
 import { Link } from "gatsby";
 import * as testiStyles from "./testimonial.module.css";
+import Data from "./content.json"
 
 /**
  * Component for a standard testimonial block module ()
@@ -12,9 +13,15 @@ import * as testiStyles from "./testimonial.module.css";
 export default function Testimonials(props){
   return (
     <div className={"testimonialsContainer"}>
-      <Testimonial img="" author="" content="" />
-      <Testimonial img="" author="" content="" />
-      <Testimonial img="" author="" content="" />
+      <Testimonial img={Data.testimonials[0].img} 
+                   author={Data.testimonials[0].author} 
+                   content={Data.testimonials[0].content} />
+      <Testimonial img={Data.testimonials[1].img} 
+                   author={Data.testimonials[1].author} 
+                   content={Data.testimonials[1].content} />
+      <Testimonial img={Data.testimonials[2].img} 
+                   author={Data.testimonials[2].author} 
+                   content={Data.testimonials[2].content} />
     </div>
   );
 }
