af2a5e2bcd33a0cab697d7f5c23c2442f3ce6c94
Tom Strat <tomstratfic@hotmail.com>
2021-04-05 20:59:37 +0100

    add more component html to footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -10,7 +10,28 @@ import * as footerStyles from "./footer.module.css";
  export default function Footer({  }) {
   return (
     <div className={footerStyles.footer}>
-      
+      <div className={footerStyles.footerColumns}>
+        <div className={footerStyles.sitemap}>
+          <p><strong>Sitemap</strong></p>
+        </div>
+        <div className={footerStyles.footerContact}>
+          <p><strong>Contact Us</strong></p>
+          <p><strong>Email:</strong>info@electrichive.com</p>
+          <p><strong>Telephone:</strong>12345 678900</p>
+          <p><strong>Address:</strong>1 Test Ave, SP9 7UF</p>
+        </div>
+        <div className={footerStyles.socialMedia}>
+          <p><strong>Get Social</strong></p>
+          <span><i>F</i></span>
+          <span><i>T</i></span>
+          <span><i>L</i></span>
+          <span><i>I</i></span>
+        </div>
+        <img src={"/logo.svg"} />
+      </div>
+      <div className={footerStyles.copyright}>
+        <p>Copyright 2021 The Electric Hive | Developed by The Electric Hive</p>
+      </div>
     </div>
   );
  }
