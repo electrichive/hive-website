7f260b673597feaf116e0fb25c56cdade4b77db0
Tom Strat <tomstratfic@hotmail.com>
2021-04-05 20:44:52 +0100

    added footer component and boilerplate

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Link from "gatsby-link";
+import * as footerStyles from "./footer.module.css";
+
+/**
+ * Component for a responsive footer (tabs and links along the bottom)
+ * @returns JSXElement 
+ */
+ export default function Footer({  }) {
+  return (
+    <div className={footerStyles.footer}>
+      
+    </div>
+  );
+ }
