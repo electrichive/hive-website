7fd304e595b9becfae09430e11f60c5489c975ba
Tom Strat <tomstratfic@hotmail.com>
2021-04-06 20:36:05 +0100

    added paralax component, boilerplate and css

---
+++ src/components/Parallax/parallax.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "../Button/button";
+import * as paraStyles from "./parallax.module.css";
+
+/**
+ * Component for a standard parallax module (has an image, content, text & link prop)
+ * @returns JSXElement 
+ */
+
+
+//testimonialImage div has inline style as the URL cannot be passed to css
+export default function Parallax(props){
+
+  return (
+    <div style={{backgroundImage:`url(${props.img})`}} className={paraStyles.paraContainer}>
+      <h3>{props.content}</h3>
+      <Button url={props.url} text={props.text} theme={"light"} />
+    </div>
+  );
+
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout/layout";
+import Slide from "../components/Slide/slide";
+import Parallax from "../components/Parallax/parallax";
 
 export default function Home() {
   return (
     <Layout>
+      <Slide title="The Electric Hive" subtitle="Subtitle Mission Statement" button="true"/>
       <p>The Main Page <span role="img" aria-label="nervous smile lol">&#128517;</span></p>
+      <Parallax url={"./mentorship"} text={"Sign Up"} img={"./img/stock-code1.jpg"} content={"Join Our Mentorship Program Now"}/>
     </Layout>
   );
 }
