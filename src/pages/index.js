eb8778655619ba8a3591839208a0d4e9a7339b6d
Tom Strat <tomstratfic@hotmail.com>
2021-04-19 15:35:41 +0100

    used css vars for all styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../components/Layout/layout";
 import Slide from "../components/Slide/slide";
 import Parallax from "../components/Parallax/parallax";
+import InfoBox from "../components/Infobox/infobox";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Slide title="The Electric Hive" subtitle="Subtitle Mission Statement" button="true"/>
       <p>The Main Page <span role="img" aria-label="nervous smile lol">&#128517;</span></p>
       <Parallax url={"./mentorship"} text={"Sign Up"} img={"./img/stock-code1.jpg"} content={"Join Our Mentorship Program Now"}/>
+      <InfoBox title={"Contribute To Our FOSSS"} description={"Lorem Ipsum"}/>
     </Layout>
   );
 }
