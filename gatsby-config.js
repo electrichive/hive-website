5fe65aef2233a267a313a988f87d4a1beadcecdc
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-09-23 14:04:38 -0700

    Standardized project formatting with new prettier config and structure with consistent file naming, hierarchy, and path aliases for a less cluttered import section in each file. This was possible with the 'gatsby-plugin-root-import' plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,28 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+const path = require('path');
 
 module.exports = {
-  /* Your site config here */
-  siteMetadata: {
-    navbar: {
-      items: [
-        ['About Us', '/about'],
-        ['FOSS', '/foss'],
-        ['Mentorship', '/mentorship'],
-        ['Contact', '/contact']
-      ]
-    }
-  },
-  plugins: [
-    "gatsby-plugin-testing"
-  ],
-}
+    /* Your site config here */
+    siteMetadata: {
+        navbar: {
+            items: [
+                ['About Us', '/about'],
+                ['FOSS', '/foss'],
+                ['Mentorship', '/mentorship'],
+                ['Contact', '/contact'],
+            ],
+        },
+    },
+    plugins: [
+        'gatsby-plugin-testing',
+        {
+            resolve: 'gatsby-plugin-root-import',
+            options: {
+                resolveModules: [path.join(__dirname, 'src', 'components')],
+                pages: path.join(__dirname, 'src', 'pages'),
+            },
+        },
+    ],
+};
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,33 +1,65 @@
-import React from "react";
-import Slide from "../components/Slide/slide";
-import Layout from "../components/Layout/layout";
-import Button from "../components/Button/button";
-import * as aboutStyles from "./styles/about.module.css";
+import React from 'react';
+import Slide from 'Slide';
+import Layout from 'Layout';
+import Button from 'Button';
+import * as styles from '../styles/pages/about.module.css';
 
 //Blocks specific to the about page so created here
-const InfoBlock = (props) => {
-  const theme = props.theme === "light" ? aboutStyles.light : aboutStyles.dark;
-  return (
-    <div className={`${theme} ${aboutStyles.infoBlock}`}>
-      <h3>{props.title}</h3>
-      <p>{props.text}</p>
-      <Button theme={props.buttonTheme} text={props.buttonText} url={props.buttonUrl}/>
-    </div>
-  );
-}
+const InfoBlock = props => {
+    const theme =
+        props.theme === 'light' ? styles.light : styles.dark;
+    return (
+        <div className={`${theme} ${styles.infoBlock}`}>
+            <h3>{props.title}</h3>
+            <p>{props.text}</p>
+            <Button
+                theme={props.buttonTheme}
+                text={props.buttonText}
+                url={props.buttonUrl}
+            />
+        </div>
+    );
+};
 
-export default function About(){
-  return (
-    <Layout>
-      <Slide title="About Us" subtitle="Subtitle Mission Statement" button="true"/>
-      <div className={aboutStyles.introContainer}>
-        <h2>Who Are The Electric Hive?</h2>
-        <p>Nullam laoreet pulvinar gravida. Aliquam auctor dolor urna, at mattis tortor dignissim vel. Proin eu velit ultrices arcu mattis consequat sit amet sed diam. Fusce odio leo, tristique quis lobortis vel, commodo id turpis. Phasellus dapibus massa in auctor venenatis. Donec erat erat, scelerisque sit amet mauris eu, euismod mattis mi. Nulla facilisi. Mauris eleifend ultrices velit ac sodales. Etiam accumsan finibus leo id hendrerit.</p>
-      </div>
-      <div className={aboutStyles.infoBlockContainer}>
-        <InfoBlock theme="light" title="Mentorship" text="Test" buttonTheme="dark" buttonText="Sign Up" buttonUrl="/mentorship" />
-        <InfoBlock theme="dark" title="Free Open Source Software (FOSS)" text="Test" buttonTheme="light" buttonText="Contribute" buttonUrl="/foss" />
-      </div>
-    </Layout>
-  );
+export default function About() {
+    return (
+        <Layout>
+            <Slide
+                title="About Us"
+                subtitle="Subtitle Mission Statement"
+                button="true"
+            />
+            <div className={styles.introContainer}>
+                <h2>Who Are The Electric Hive?</h2>
+                <p>
+                    Nullam laoreet pulvinar gravida. Aliquam auctor dolor urna,
+                    at mattis tortor dignissim vel. Proin eu velit ultrices arcu
+                    mattis consequat sit amet sed diam. Fusce odio leo,
+                    tristique quis lobortis vel, commodo id turpis. Phasellus
+                    dapibus massa in auctor venenatis. Donec erat erat,
+                    scelerisque sit amet mauris eu, euismod mattis mi. Nulla
+                    facilisi. Mauris eleifend ultrices velit ac sodales. Etiam
+                    accumsan finibus leo id hendrerit.
+                </p>
+            </div>
+            <div className={styles.infoBlockContainer}>
+                <InfoBlock
+                    theme="light"
+                    title="Mentorship"
+                    text="Test"
+                    buttonTheme="dark"
+                    buttonText="Sign Up"
+                    buttonUrl="/mentorship"
+                />
+                <InfoBlock
+                    theme="dark"
+                    title="Free Open Source Software (FOSS)"
+                    text="Test"
+                    buttonTheme="light"
+                    buttonText="Contribute"
+                    buttonUrl="/foss"
+                />
+            </div>
+        </Layout>
+    );
 }
