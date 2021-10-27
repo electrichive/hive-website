83e7f44dd9a313d27d431ab3035b73e785283e22
Tom Strat Laptop <tomstratfic@hotmail.com>
2021-10-27 18:44:59 +0100

    add infoimage styles

--- src/components/infoimage/infoimages.js
+++ src/components/infoimage/infoimages.js
@@ -12,16 +12,17 @@ import Data from "./content.json"
 
 export default function InfoImages(){
 
+  //Perhaps find a loop for this
   return (
     <div className={iiStyles.infoimagesContainer}>
-      <InfoImage img={Data.infoimages[chosen[0]].img} 
-                   content={Data.infoimages[chosen[0]].content}
+      <InfoImage img={Data.infoimages[0].img} 
+                   content={Data.infoimages[0].content}
                    direction="left" />
-      <InfoImage img={Data.infoimages[chosen[1]].img} 
-                   content={Data.infoimages[chosen[1]].content}
+      <InfoImage img={Data.infoimages[1].img} 
+                   content={Data.infoimages[1].content}
                    direction="right" />
-      <InfoImage img={Data.infoimages[chosen[2]].img} 
-                   content={Data.infoimages[chosen[2]].content}
+      <InfoImage img={Data.infoimages[2].img}
+                   content={Data.infoimages[2].content}
                    direction="left" />
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,13 @@ import Layout from "../components/Layout/layout";
 import Slide from "../components/Slide/slide";
 import Parallax from "../components/Parallax/parallax";
 import InfoBox from "../components/Infobox/infobox";
+import InfoImages from "../components/infoimage/infoimages";
 
 export default function Home() {
   return (
     <Layout>
       <Slide title="The Electric Hive" subtitle="Subtitle Mission Statement" button="true"/>
-      <p>The Main Page <span role="img" aria-label="nervous smile lol">&#128517;</span></p>
+      <InfoImages />
       <Parallax url={"./mentorship"} text={"Sign Up"} img={"./img/stock-code1.jpg"} content={"Join Our Mentorship Program Now"}/>
       <InfoBox title={"Contribute To Our FOSSS"} description={"Lorem Ipsum"} theme={"dark"}/>
     </Layout>
