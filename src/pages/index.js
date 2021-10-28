cc27e4cfcce2ca669191a3f2c1a3792b476efe57
Tom Strat Laptop <tomstratfic@hotmail.com>
2021-10-28 10:37:41 +0100

    add JSON content for pages and styling for infobox

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,10 @@ import Layout from "../components/Layout/layout";
 import Slide from "../components/Slide/slide";
 import Parallax from "../components/Parallax/parallax";
 import InfoBox from "../components/Infobox/infobox";
-import InfoImages from "../components/infoimage/infoimages";
+import InfoImages from "../components/Infoimage/infoimages";
+import Content from "./content/home.json";
+
+//Content for blocks is pulled from JSON in the folder "content"
 
 export default function Home() {
   return (
@@ -11,7 +14,7 @@ export default function Home() {
       <Slide title="The Electric Hive" subtitle="Subtitle Mission Statement" button="true"/>
       <InfoImages />
       <Parallax url={"./mentorship"} text={"Sign Up"} img={"./img/stock-code1.jpg"} content={"Join Our Mentorship Program Now"}/>
-      <InfoBox title={"Contribute To Our FOSSS"} description={"Lorem Ipsum"} theme={"dark"}/>
+      <InfoBox title={"Contribute To Our FOSS"} description={Content.infobox.description} theme={"dark"}/>
     </Layout>
   );
 }
