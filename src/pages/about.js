23e1e40db14d5f880c98b9840cade72291752dbc
Tom Strat Laptop <tomstratfic@hotmail.com>
2021-10-28 17:19:13 +0100

    add intro component and updated about page to use instead

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Intro from "../components/Intro/intro";
 import Slide from "../components/Slide/slide";
 import Layout from "../components/Layout/layout";
 import Button from "../components/Button/button";
@@ -22,10 +23,7 @@ export default function About(){
   return (
     <Layout>
       <Slide title="About Us" subtitle="Subtitle Mission Statement" button="true"/>
-      <div className={aboutStyles.introContainer}>
-        <h2>Who Are The Electric Hive?</h2>
-        <p>Nullam laoreet pulvinar gravida. Aliquam auctor dolor urna, at mattis tortor dignissim vel. Proin eu velit ultrices arcu mattis consequat sit amet sed diam. Fusce odio leo, tristique quis lobortis vel, commodo id turpis. Phasellus dapibus massa in auctor venenatis. Donec erat erat, scelerisque sit amet mauris eu, euismod mattis mi. Nulla facilisi. Mauris eleifend ultrices velit ac sodales. Etiam accumsan finibus leo id hendrerit.</p>
-      </div>
+      <Intro title={Content.intro.title} content={Content.intro.content} />
       <div className={aboutStyles.infoBlockContainer}>
         <InfoBlock theme="light" title={Content.infoblocks[0].title} text={Content.infoblocks[0].text} buttonTheme="dark" buttonText="Sign Up" buttonUrl="/mentorship" />
         <InfoBlock theme="dark" title={Content.infoblocks[1].title} text={Content.infoblocks[1].text} buttonTheme="light" buttonText="Contribute" buttonUrl="/foss" />
