4766f7e1ea7bba841676c510648361b86d1064ac
Tom Strat Laptop <tomstratfic@hotmail.com>
2021-10-28 17:22:00 +0100

    add mentorship page and add content for intro

--- src/pages/mentorship.js
+++ src/pages/mentorship.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout/layout";
 import Slide from "../components/Slide/slide";
+import Intro from "../components/Intro/intro";
 import Parallax from "../components/Parallax/parallax";
 import InfoBox from "../components/Infobox/infobox";
 import InfoImages from "../components/Infoimage/infoimages";
@@ -12,6 +13,7 @@ export default function Mentorship() {
   return (
     <Layout>
       <Slide title="Mentorship Program" subtitle="Subtitle Mission Statement" button="true"/>
+      <Intro title={Content.intro.title} content={Content.intro.content} />
       <InfoImages />
       <Parallax url={"./mentorship"} text={"Sign Up"} img={"./img/stock-code1.jpg"} content={"Join Our Mentorship Program Now"}/>
       <InfoBox title={"Contribute To Our FOSS"} description={Content.infobox.description} theme={"dark"}/>
