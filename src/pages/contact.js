bc8016eaa0e1ac0e1855719ac24310e46b0c94c7
Tom Strat Laptop <tomstratfic@hotmail.com>
2021-10-28 17:02:24 +0100

    add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout/layout";
+
+
+//Content for blocks is pulled from JSON in the folder "content"
+
+export default function Contact() {
+  return (
+    <Layout>
+
+    </Layout>
+  );
+}
