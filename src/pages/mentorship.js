a6b8aec84efa0c218351342e9935d32532c524e9
Tom Strat Laptop <tomstratfic@hotmail.com>
2021-10-28 17:37:25 +0100

    add mentorship content

--- src/pages/mentorship.js
+++ src/pages/mentorship.js
@@ -3,9 +3,7 @@ import Layout from "../components/Layout/layout";
 import Slide from "../components/Slide/slide";
 import Intro from "../components/Intro/intro";
 import Parallax from "../components/Parallax/parallax";
-import InfoBox from "../components/Infobox/infobox";
-import InfoImages from "../components/Infoimage/infoimages";
-import Content from "./content/home.json";
+import Content from "./content/mentorship.json";
 
 //Content for blocks is pulled from JSON in the folder "content"
 
@@ -14,7 +12,7 @@ export default function Mentorship() {
     <Layout>
       <Slide title="Mentorship Program" subtitle="Subtitle Mission Statement" button="true"/>
       <Intro title={Content.intro.title} content={Content.intro.content} />
-      <Parallax url={"./mentorship"} text={"Sign Up"} img={"./img/stock-code1.jpg"} content={"Join Our Mentorship Program Now"}/>
+      <Parallax url={"./resources"} text={"View Our Resources"} img={"./img/stock-code1.jpg"}/>
       <Testimonials />
     </Layout>
   );
