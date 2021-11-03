fd1fde291a03ac8c8c7d801eeb130f19d6a3be84
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-11-03 09:00:08 +0000

    Convert CSS module to styled-components implementation for Testimonial component

---
+++ src/components/Testimonial/testimonial.styled.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+export const Image = styled.img``;
+export const Title = styled.h3``;
+export const Content = styled.p``;
