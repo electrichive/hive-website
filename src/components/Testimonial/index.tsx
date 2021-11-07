81749db7e6921e948b37299d93850b7c8193faad
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-11-07 22:01:41 +0000

    Implement Testimonial(s) typescript with styled components

--- src/components/Testimonial/index.tsx
+++ src/components/Testimonial/index.tsx
@@ -1,33 +1,79 @@
-import { Container, Image, Title, Content } from './testimonial.styled';
+import Data from './content.json';
+import {
+    Container,
+    Image,
+    Title,
+    Content,
+    HexContainer,
+    ContentContainer,
+    ImageBg,
+    Author,
+} from './testimonial.styled';
+
+function randomNumberGen(): number {
+    //Gets random number between 0 and total number of testimonials
+    return Math.floor(Math.random() * Data.testimonials.length);
+}
+
+function randomTestimonialPicker(): number[] {
+    const chosen: number[] = []; // Will hold the 3 chosen testimonials
+
+    while (chosen.length < 3) {
+        const randomNumber = randomNumberGen();
+        let alreadyTaken = false;
+
+        // Loop will (crudely) pick 3 unique entries retrying if it gets one wrong
+        // Worth revisiting when we have a large number of testimonials
+        if (chosen.length === 0) {
+            chosen.push(randomNumber);
+        } else {
+            chosen.forEach(num => {
+                if (randomNumber === num) {
+                    alreadyTaken = true;
+                }
+            });
+            if (!alreadyTaken) {
+                chosen.push(randomNumber);
+            }
+        }
+    }
+    return chosen;
+}
 
 /**
  * Component for a standard testimonial module (has an image name and text prop)
  * is passed data from the testimonials module
  */
-export default function Testimonial({
-    img,
-    title,
-    content,
-}: TestimonialProps): JSX.Element {
+export default function Testimonial(props: TestimonialProps): JSX.Element {
     return (
         <Container>
-            <Image src={img} alt="Testimonial" />
-            <Title>{title}</Title>
-            <Content>{content}</Content>
+            <HexContainer direction={props.direction}>
+                <Image img={props.img} alt="Testimonial" />
+                <ImageBg direction={props.direction} />
+                <Title>{props.title}</Title>
+            </HexContainer>
+            <ContentContainer>
+                <Content>{props.content}</Content>
+                <Author>{props.author}</Author>
+            </ContentContainer>
         </Container>
     );
 }
 
-/**
- * @param testimonials - list of props for a testimonial component (img, title,
- * content attributes)
- */
-export function Testimonials(testimonials: TestimonialProps[]): JSX.Element {
+export function Testimonials(): JSX.Element {
+    const chosenIdxs = randomTestimonialPicker();
     return (
         <Container>
-            {testimonials.map((testimonial, i) => {
-                <Testimonial key={i} {...testimonial} />;
-            })}
+            <h2>What do others think?</h2>
+            {chosenIdxs
+                .map(idx => Data.testimonials[idx])
+                .map((testimonial, i) => {
+                    <Testimonial
+                        key={i}
+                        direction={i % 2 === 0 ? 'left' : 'right'}
+                        {...testimonial}
+                    />;
+                })}
         </Container>
     );
 }
