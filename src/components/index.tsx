e0a72a6885837a155ae2a1153ac5f05ba6ffa6d7
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-11-07 20:45:34 +0000

    Add helper importer for succinct import {} from 'components' syntax

---
+++ src/components/index.tsx
@@ -0,0 +1,14 @@
+export { default as Button } from './Button';
+export { default as Contact } from './Contact';
+export { default as Footer } from './Footer';
+export { default as Header } from './Header';
+export { default as Heximage } from './Heximage';
+export { default as Honeycomb } from './Honeycomb';
+export { default as Infobox } from './Infobox';
+export { default as Infoimage } from './Infoimage';
+export { default as Intro } from './Intro';
+export { default as Layout } from './Layout';
+export { default as Parallax } from './Parallax';
+export { default as SEO } from './SEO';
+export { default as Slide } from './Slide';
+export { default as Testimonial } from './Testimonial';
