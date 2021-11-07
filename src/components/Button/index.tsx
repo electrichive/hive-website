2e54ffe81eb600d07a71daa0942aca982e52e7dd
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-11-07 20:50:10 +0000

    Add more explicit and readable syntax.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,5 +4,7 @@ import { StyledLink } from './button.styled';
  * Component for a standard button (has url, theme and text prop)
  */
 export default function Button(props: ButtonProps): JSX.Element {
-    return <StyledLink to={props.url ?? '/'}>{props.text ?? ' '}</StyledLink>;
+    const url = props.url ?? '/';
+    const text = props.text ?? '';
+    return <StyledLink to={url}>{text}</StyledLink>;
 }
