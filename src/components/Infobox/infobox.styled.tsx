470b9ba78f32a8865a291328004ae4a4c91888f1
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-11-07 21:33:44 +0000

    Convert infobox css module to styled typescript

---
+++ src/components/Infobox/infobox.styled.tsx
@@ -0,0 +1,22 @@
+import { darkThemeMixin, lightThemeMixin } from 'src/styles/mixins.styled';
+import styled from 'styled-components';
+
+export const InfoBlock = styled.div<Pick<InfoBoxProps, 'theme'>>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 100px 150px;
+    width: 100%;
+    text-align: center;
+
+    @media only screen and (max-width: 800px) {
+        padding: 100px 30px;
+    }
+
+    ${props => (props.theme === 'dark' ? darkThemeMixin : lightThemeMixin)}
+`;
+
+export const Content = styled.div``;
+export const Title = styled.h2``;
+export const Description = styled.p``;
