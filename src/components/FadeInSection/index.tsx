5590f3c69c8816933c65ab25dbfe89dc61c4ada0
Tom Copestake <tom.copestake@moneyhub.com>
2022-10-15 21:42:38 +0000

    Update fade component css

--- src/components/FadeInSection/index.tsx
+++ src/components/FadeInSection/index.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 const FadeInWrapper = styled.div`
     width: 100%;
+    display: flex;
+    justify-content: center;
 `;
 
 export default function FadeInSection({
