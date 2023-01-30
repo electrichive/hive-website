2af3ab4c6b72fd5236fba0befc98f083b7f3d9fb
Tristan Schrader <tristan@climaxfoods.com>
2023-01-29 21:23:30 -0800

    feat [wip]: initialize aws vpc network for project

--- infra/config.nix
+++ infra/config.nix
@@ -4,7 +4,6 @@ let
   project = "website";
 in
 {
-
   # TERRAFORM CONFIG
   terraform.backend.s3 = {
     bucket = "${org}-terraform";
@@ -12,11 +11,28 @@ in
   };
   provider.aws = {
     region = "us-west-1";
+    profile = "${org}_tristan";
   };
 
-  # EXISTING RESOURCES
+  data.aws_organizations_organization.electrichive_org = {};
 
-  # NEW RESOURCES
-
+  resource.aws_vpc_ipam.main = {
+    operating_regions  = {
+      region_name = config.provider.aws.region;
+    };
+  };
+  resource.aws_vpc_ipam_pool.main = {
+    address_family = "ipv4";
+    ipam_scope_id  = "\${ aws_vpc_ipam.main.private_default_scope_id }";
+    locale         = config.provider.aws.region;
+  };
+  resource.aws_vpc_ipam_pool_cidr.main = {
+    ipam_pool_id = "\${ aws_vpc_ipam_pool.main.id }";
+    cidr         = "172.2.0.0/16";
+  };
+  resource.aws_vpc.main = {
+    ipv4_ipam_pool_id = "\${ aws_vpc_ipam_pool.main.id }";
+    ipv4_netmask_length = 28;
+  };
 }
 
